b41aaadaca481fafa050ca4b86dd92d166fd5025
Naumenko Semen <semenkan2009@gmail.com>
2026-09-11 00:53:25 +0300

    Lab02:string to number conversion, Parse vs TryParse

--- obj/Debug/net10.0/Lab02-Naumenko.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Naumenko.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Naumenko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+408894a80e500c3cdfd4ac1ab855aa210b0fbf29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4a63520e997ca661bf8483a21cb26134c19433")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Naumenko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Naumenko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
