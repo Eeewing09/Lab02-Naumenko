dbcc92349261e30a5530bd1287b43d05207a2bdb
Naumenko Semen <semenkan2009@gmail.com>
2026-09-11 00:26:43 +0300

    Lab02: char and decimal precision

--- obj/Debug/net10.0/Lab02-Naumenko.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Naumenko.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Naumenko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e046d318b620374dbabc65a1f8278095752d602")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Naumenko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Naumenko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
